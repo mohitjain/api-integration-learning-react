c3e473541899ee8fdc2e2d290c057377bf9387ec
Mohit Jain <jain.mohit27@gmail.com>
2020-08-19 15:50:52 +0530

    Request and Response, Global Interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -25,6 +26,9 @@ class Blog extends Component {
                 })
                 this.setState({posts: updatedPosts});
             })
+            .catch(error => {
+                this.setState({ error: true });
+            });
     }
 
     postSelectedHandler = (id) => {
@@ -32,14 +36,18 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title}
-                key={post.id}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        })
+        let posts = <p style={{textAlign: 'center' }}> Something went wrong </p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    key={post.id}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            });
+        }
+
 
         return (
             <div>
