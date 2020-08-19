c9c153be041f23ab3c83af6d2e67a1bbcc92e3de
Mohit Jain <jain.mohit27@gmail.com>
2020-08-19 14:31:29 +0530

    Get, Post, Delete a basic request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -26,9 +27,18 @@ class Blog extends Component {
             })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} key={post.id} author={post.author}/>
+            return <Post
+                title={post.title}
+                key={post.id}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
         })
 
         return (
@@ -37,7 +47,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
